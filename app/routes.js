1105c6c884f4be8556d6277e1fc81dbef3bfef71
Rafael Felipe <rafafelipesilva@gmail.com>
2018-06-07 00:04:03 -0300

    API REST

---
+++ app/routes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const requireDir = require('require-dir');
+
+
+const routes = express.Router();
+
+const authMiddleware = require('./middlewares/auth');
+
+const controllers = requireDir('./controllers');
+
+// Auth
+routes.post('/signup', controllers.authController.signup);
+routes.post('/signin', controllers.authController.signin);
+
+// Auth routes
+routes.use(authMiddleware);
+
+// Users
+routes.put('/users', controllers.userController.update);
+routes.get('/users/me', controllers.userController.me);
+routes.get('/feed', controllers.userController.feed);
+
+// Follows
+routes.post('/follow/:id', controllers.followController.create);
+routes.delete('/unfollow/:id', controllers.followController.destroy);
+
+// Tweets
+routes.post('/tweets', controllers.tweetController.create);
+routes.delete('/tweets/:id', controllers.tweetController.destroy);
+
+// Likes
+routes.post('/like/:id', controllers.likeController.toggle);
+
+
+module.exports = routes;
