f103f61d6ae40abaaf7a5593bbaee81f68008d6a
Rafael Felipe <rafafelipesilva@gmail.com>
2018-06-25 23:09:31 -0300

    Signup tests

--- __tests__/integration/auth.test.js
+++ __tests__/integration/auth.test.js
@@ -3,6 +3,15 @@ const app = require('../../index');
 const mongoose = require('mongoose');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const factory = require('../factories');
+const sinon = require('sinon');
+const nodemailer = require('nodemailer');
+
+const transport = {
+  sendMail: sinon.spy(),
+};
+
+sinon.stub(nodemailer, 'createTransport').returns(transport);
 
 const { expect } = chai;
 
@@ -15,12 +24,38 @@ describe('Authentication', () => {
     await User.remove();
   });
 
+  describe('Sign up', () => {
+    it('it should be able to sign up', async () => {
+      const user = await factory.attrs('User');
+
+      const response = await chai.request(app)
+        .post('/api/signup')
+        .send(user);
+
+      expect(response.body).to.have.property('user');
+      expect(response.body).to.have.property('token');
+      // expect(transport.sendMail.calledOnce).to.be.true;
+    });
+
+    it('it should not be able to sign up with duplicates', async () => {
+      const user = await factory.create('User');
+
+      const user2 = await factory.attrs('User', {
+        email: user.email,
+      });
+
+      const response = await chai.request(app)
+        .post('/api/signup')
+        .send(user2);
+
+      expect(response).to.have.status(400);
+      expect(response.body).to.have.property('error');
+    });
+  });
+
   describe('Sign in', () => {
     it('it should be able to authenticate with valid credentials', async () => {
-      const user = await User.create({
-        name: 'Rafa',
-        username: 'rfs',
-        email: 'rfs@gmail.com',
+      const user = await factory.create('User', {
         password: '123456',
       });
 
@@ -42,10 +77,7 @@ describe('Authentication', () => {
     });
 
     it('no authenticate with different password', async () => {
-      const user = await User.create({
-        name: 'Rafa',
-        username: 'rfs',
-        email: 'rfs@gmail.com',
+      const user = await factory.create('User', {
         password: '123456',
       });
 
